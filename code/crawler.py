b33e6c2ae71fa383b14b69f566cde8e9a8995fff
ShirleyMao2023 <55336911+ShirleyMao2023@users.noreply.github.com>
2021-11-14 02:06:22 -0600

    more articles

--- code/crawler.py
+++ code/crawler.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 from sys import platform
 import os
+import re 
 if platform == "linux" or platform == "linux2":
     from pyvirtualdisplay import Display
 
@@ -15,6 +16,7 @@ Courtesy from scraper.ipynb by Bhavya
 
 # pip install bs4 selenium
 class JavaScript_scrape():
+     
     """
     scrapping javascript based website
     """
@@ -44,7 +46,7 @@ class JavaScript_scrape():
 
         #chrome driver version and system
         driver_version = "95.0.4638.54"
-        driver_os = ""
+        driver_os = "mac"
 
         if platform == "linux" or platform == "linux2":
             # linux
@@ -65,8 +67,9 @@ class JavaScript_scrape():
         if platform == "linux" or platform == "linux2":
             display = Display(visible=0, size=(800, 800))  
             display.start()
+        driver = webdriver.Chrome('./chromedriver',options=options)
 
-        driver = webdriver.Chrome(f'./chromedriver' + '-' + driver_version + '-' + driver_os, options=options)
+        # driver = webdriver.Chrome(f'./chromedriver' + '-' + driver_version + '-' + driver_os, options=options)
         driver.get(url)
         res_html = driver.execute_script("return document.body.innerHTML")
         soup = BeautifulSoup(res_html, 'html.parser')  # beautiful soup object to be used for parsing html content
@@ -80,7 +83,7 @@ class JavaScript_scrape():
         for script in soup(["script", "style"]):
             script.decompose()
 
-        return soup        
+        return soup     
 
     def process_output(self, articles=[], output='list', source="unknown"):
         """
@@ -106,7 +109,35 @@ class JavaScript_scrape():
                     f.write(article)
 
         return articles
+    def process_output_mac(self, articles=[], output='list', source="unknown"):
+        """
+        Processes the output according to the user needs
+        """
+        if output == 'txt':
+            curr_path = os.getcwd()
+            output_path = curr_path + '/articles'
+            
+            if len(articles) == 0:
+                return []
 
+            #make directory articles if it does not exist 
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+
+            #iterate through articles and print them out
+            for i, article in enumerate(articles):                
+                if len(article) < 1:
+                    continue
+                
+                with open(output_path+'/'+source+'-'+str(i)+'.txt', 'w') as f:
+                    f.write(article)
+
+        return articles
+    def process_text(self, text):
+        text = text.encode('ascii',errors='ignore').decode('utf-8')       #removes non-ascii characters
+        # text = re.sub('\s{2,}','\n',text)       #repalces repeated whitespace characters with single space
+        # text = re.sub('Text Body: ','',text)
+        return text 
     def removeNonAsciiCharacters(self, string):
         return string.encode('ascii',errors='ignore').decode('utf-8')   #removes non-ascii characters
 
@@ -165,7 +196,6 @@ class JavaScript_scrape():
         news_links = []
         news_url = 'https://www.aappublications.org/news'
         base_url = 'https://www.aappublications.org/'
-
         # scrape to find news links in the base url
         soup = self.get_js_soup(news_url)
 
@@ -206,10 +236,103 @@ class JavaScript_scrape():
                 articles.append(article)
 
         return self.process_output(articles=articles, output=output, source='aapNews')
-        
+
+
+    def scrape_medscape(self, output='list'):
+        """
+        extracts newest articles from medical news today
+        """
+        news_links = []
+        base_url = 'https://www.medscape.com/'
+
+        # scrape to find news links in the base url
+        soup = self.get_js_soup(base_url)
+
+        for i, link_holder in enumerate(soup.find_all('a')):
+            if (link_holder.has_attr('href')):
+                rel_link = link_holder['href'] #get url
+                #url returned is relative, so we need to add base url
+                if ('viewarticle' not in rel_link):
+                    continue
+                if (rel_link.startswith('//')):
+                    rel_link = rel_link[rel_link.find('//') + 2:]
+                if ('medscape' not in rel_link and '//' not in rel_link and rel_link.find('/') == 0):
+                    rel_link = 'https://www.medscape.com' + rel_link
+                if ('https' not in rel_link):
+                    rel_link = 'https://' + rel_link
+                news_links.append(rel_link) 
+
+        print('Found Medscape Links', len(news_links))
+
+        articles = []
+        # scrape articles
+        for link in news_links:
+            #url returned is relative, so we need to add base url
+            print ('LINK:{}'.format(link))
+            soup = self.get_js_soup(link)
+            article = ""
+
+            #get div that contains the article
+            article_holder = soup.find('div',class_='article-wrapper')
+
+            if article_holder == None:
+                continue 
+
+            #get article titles
+            article = self.process_text(article_holder.get_text(separator=' '))
+            if len(article.strip()) > 0:
+                articles.append(article)
+
+        return self.process_output_mac(articles=articles, output=output, source='medscape')    
+
+    def scrape_webMD(self, output='list'):
+        """
+        extracts newest articles from medical news today
+        """
+        news_links = []
+        base_url = 'https://www.webmd.com/news/default.htm'
+
+        # scrape to find news links in the base url
+        soup = self.get_js_soup(base_url)
+
+        for link_holder in soup.find_all('li'): #get list of all <li>
+            a_tag = link_holder.find('a')
+            if (a_tag != None):
+                if (a_tag.has_attr('href')):
+                    rel_link = a_tag['href'] #get url
+                    #url returned is relative, so we need to add base url
+                    if ('default' in rel_link or 'app' in rel_link or 'slideshow' in rel_link or 'quiz' in rel_link):
+                        continue
+                    if (rel_link.startswith('//')):
+                        rel_link = "https:" + rel_link
+                    news_links.append(rel_link) 
+
+        print('Found WebMD Links', len(news_links))
+
+        articles = []
+        # scrape articles
+        for link in news_links:
+            print ('LINK:{}'.format(link))
+            #url returned is relative, so we need to add base url
+            soup = self.get_js_soup(link)
+            article = ""
+
+            #get div that contains the article
+            article_holder = soup.find('div',class_='article__body')
+
+            if article_holder == None:
+                continue 
+
+            article = self.process_text(article_holder.get_text(separator=' '))
+            
+            if len(article.strip()) > 0:
+                articles.append(article)
+
+        return self.process_output_mac(articles=articles, output=output, source='webmd')   
 
 if __name__ == '__main__':
-    items = JavaScript_scrape().scrape_aapNews(limit=1, output='txt')
+    # items = JavaScript_scrape().scrape_webMD(output='txt')
+    # items = JavaScript_scrape().scrape_medscape(output='txt')
     print(items)
     print('articles', len(items))
 
